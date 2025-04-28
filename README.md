cad2d25581afbaa298ced2a25d8e113f4f80b963
meDigital Development <medigital.net@gmail.com>
2025-04-28 10:02:05 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Offcanvas Bootstrap 4
-Offcanvas base bootstrap 4
+# Offcanvas Plugin
+
+Offcanvas Plugin adalah sebuah plugin jQuery untuk membuat elemen offcanvas dengan dukungan backdrop dan tombol tutup. Plugin ini memungkinkan pengguna untuk menampilkan, menyembunyikan, dan men-toggle elemen offcanvas dengan mudah.
+
+## Instalasi
+
+1. Unduh file `offcanvas.js` atau salin kode di bawah ini ke file JavaScript Anda.
+
+   ```html
+   <script src="path/to/offcanvas.js"></script>
+   ```
+
+2. Pastikan Anda telah memuat jQuery sebelum menggunakan plugin ini.
+
+   ```html
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   ```
+
+## Cara Penggunaan
+
+### Menampilkan Offcanvas
+
+Tambahkan elemen dengan kelas `offcanvas`, dan gunakan metode `.offcanvas('show')` untuk menampilkan elemen tersebut.
+
+```html
+<div class="offcanvas">
+  <!-- Konten offcanvas -->
+</div>
+```
--- src/offcanvas-bs4.js
+++ src/offcanvas-bs4.js
@@ -1,59 +1,63 @@
-(function($) {
-    $.fn.offcanvas = function(action) {
-        const $offcanvas = this;
-        let $backdrop = $('.offcanvas-backdrop');
+(function ($) {
+  $.fn.offcanvas = function (action) {
+    const $offcanvas = this;
+    let $backdrop = $(".offcanvas-backdrop");
 
-        function createBackdrop() {
-            if ($backdrop.length === 0) {
-                $backdrop = $('<div class="offcanvas-backdrop"></div>');
-                $('body').append($backdrop);
-            }
+    function createBackdrop() {
+      if ($backdrop.length === 0) {
+        $backdrop = $('<div class="offcanvas-backdrop"></div>');
+        $("body").append($backdrop);
+      }
+    }
+
+    function show() {
+      createBackdrop();
+      $offcanvas.addClass("show");
+      $backdrop.removeClass("d-none");
+    }
+
+    function hide() {
+      $offcanvas.removeClass("show");
+      $backdrop.addClass("d-none");
+    }
+
+    function toggle() {
+      createBackdrop();
+      if ($offcanvas.hasClass("show")) {
+        hide();
+      } else {
+        show();
+      }
+    }
+
+    // Klik backdrop untuk close
+    $(document)
+      .off("click.offcanvas")
+      .on("click.offcanvas", ".offcanvas-backdrop", function () {
+        hide();
+      });
+
+    // Tekan ESC untuk close
+    $(document)
+      .off("keydown.offcanvas")
+      .on("keydown.offcanvas", function (event) {
+        if (event.key === "Escape" && $offcanvas.hasClass("show")) {
+          hide();
         }
+      });
 
-        function show() {
-            createBackdrop();
-            $offcanvas.addClass('show');
-            $backdrop.removeClass('d-none');
-        }
+    // Jalankan aksi
+    if (action === "show") show();
+    else if (action === "hide") hide();
+    else if (action === "toggle") toggle();
 
-        function hide() {
-            $offcanvas.removeClass('show');
-            $backdrop.addClass('d-none');
-        }
+    return this; // for chaining
+  };
 
-        function toggle() {
-            createBackdrop();
-            if ($offcanvas.hasClass('show')) {
-                hide();
-            } else {
-                show();
-            }
-        }
-
-        // Klik backdrop untuk close
-        $(document).off('click.offcanvas').on('click.offcanvas', '.offcanvas-backdrop', function() {
-            hide();
-        });
-
-        // Tekan ESC untuk close
-        $(document).off('keydown.offcanvas').on('keydown.offcanvas', function(event) {
-            if (event.key === "Escape" && $offcanvas.hasClass('show')) {
-                hide();
-            }
-        });
-
-        // Jalankan aksi
-        if (action === 'show') show();
-        else if (action === 'hide') hide();
-        else if (action === 'toggle') toggle();
-
-        return this; // for chaining
-    };
-
-    // Event listener: klik button data-toggle="offcanvas"
-    $(document).on('click', '[data-toggle="offcanvas"]', function(e) {
-        e.preventDefault();
-        const target = $(this).data('target');
-        $(target).offcanvas('toggle');
-    });
+  // Event listener: klik button data-toggle="offcanvas"
+  $(document).on("click", '[data-toggle="offcanvas"]', function (e) {
+    e.preventDefault();
+    const target = $(this).data("target");
+    $(target).offcanvas("toggle");
+  });
 })(jQuery);
