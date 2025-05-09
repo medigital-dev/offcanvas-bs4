ee030ae669bdd7353b0b777b1c4366746209b0a6
meDigital Development <medigital.net@gmail.com>
2025-05-09 21:08:01 +0700

    Update doc

--- src/offcanvas-bs4.js
+++ src/offcanvas-bs4.js
@@ -1,28 +1,35 @@
 (function ($) {
-  $.fn.offcanvas = function (action) {
+  $.fn.offcanvas = function (action, config = {}) {
     const $offcanvas = this;
-    let $backdrop = $(".offcanvas-backdrop");
 
+    let $backdrop = $(".offcanvas-backdrop");
     function createBackdrop() {
       if ($backdrop.length === 0) {
         $backdrop = $('<div class="offcanvas-backdrop"></div>');
         $("body").append($backdrop);
+
+        // Klik backdrop untuk menutup
+        $backdrop.on("click", function () {
+          hide();
+        });
       }
+      return $backdrop;
     }
 
     function show() {
       createBackdrop();
       $offcanvas.addClass("show");
+      $("body").addClass("offcanvas-open");
       $backdrop.removeClass("d-none");
     }
 
     function hide() {
       $offcanvas.removeClass("show");
+      $("body").removeClass("offcanvas-open");
       $backdrop.addClass("d-none");
     }
 
     function toggle() {
-      createBackdrop();
       if ($offcanvas.hasClass("show")) {
         hide();
       } else {
@@ -30,21 +37,17 @@
       }
     }
 
-    // Klik backdrop untuk close
-    $(document)
-      .off("click.offcanvas")
-      .on("click.offcanvas", ".offcanvas-backdrop", function () {
-        hide();
-      });
-
-    // Tekan ESC untuk close
-    $(document)
-      .off("keydown.offcanvas")
-      .on("keydown.offcanvas", function (event) {
-        if (event.key === "Escape" && $offcanvas.hasClass("show")) {
-          hide();
+    // Tekan ESC untuk close (bind hanya sekali di awal)
+    if (!$(document).data("offcanvas-esc-bound")) {
+      $(document).on("keydown.offcanvas", function (event) {
+        if (event.key === "Escape") {
+          $(".offcanvas.show").each(function () {
+            $(this).offcanvas("hide");
+          });
         }
       });
+      $(document).data("offcanvas-esc-bound", true);
+    }
 
     // Jalankan aksi
     if (action === "show") show();
@@ -54,10 +57,18 @@
     return this; // for chaining
   };
 
-  // Event listener: klik button data-toggle="offcanvas"
+  // Event listener: klik button
   $(document).on("click", '[data-toggle="offcanvas"]', function (e) {
     e.preventDefault();
+    let config = {};
     const target = $(this).data("target");
+    const position = $(this).data("position");
+    console.log(position);
+
+    if (position !== undefined) {
+      $(target).addClass(".offcanvas-" + position);
+    }
+
     $(target).offcanvas("toggle");
   });
 })(jQuery);
